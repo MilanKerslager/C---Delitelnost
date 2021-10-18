d55b7c487dc3fda2d2ea3b4d60f06a9f2b7b7f27
milan.kerslager <milan.kerslager@B000-01>
2021-10-18 13:49:58 +0200

    Add project files.

---
+++ Dělitelnost čísla/Dělitelnost čísla.cpp
@@ -0,0 +1,19 @@
+﻿// Dělitelnost čísla
+// Programzjistí, zda je číslo x dělítelné číslem a
+
+#include <stdio.h>
+
+int main()
+{
+	int x = 14;
+	int delitel = 5;
+	printf("Cislo %d ", x);
+	// rozhodneme, jestli je cislo sude/liche
+	if (x % delitel == 0) {
+		printf("je");
+	}
+	else {
+		printf("neni");
+	}
+	printf(" delitelne cislem %d.\n", delitel);
+}
